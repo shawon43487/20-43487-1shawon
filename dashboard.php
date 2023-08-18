2b72829adde9a6799127e3561c7f85ab1ee691e9
shawon43487 <135755951+shawon43487@users.noreply.github.com>
2023-08-19 00:45:18 +0600

    Lab 6

---
+++ dashboard.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table border="1" cellspacing="0" height="50px" width="100%">
+        <tr>
+            <td style="text-align: right;">
+                <a href="">Home</a>
+                <p style="display: inline;">|</p>
+                <a href="">Login</a>
+                <p style="display: inline;">|</p>
+                <a href="registration.html">Registration</a>
+            </td>
+        </tr>
+        </table>
+        <table border="1" cellspacing="0" height="250px" width="100%">
+        <tr>
+            <td width="250px">
+                <h1>Account</h1><hr>
+                <ul>
+                    <li>
+                        <a href="">Dashboard</a>
+                    </li>
+                    <li>
+                        <a href="">View Profile</a>
+                    </li>
+                    <li>
+                        <a href="">Edit Profile</a>
+                    </li>
+                    <li>
+                        <a href="">Change PRofile Picture</a>
+                    </li>
+                    <li>
+                        <a href="">Change Password</a>
+                    </li>
+                    <li>
+                        <a href="">Logout</a>
+                    </li>
+                </ul>
+            </td>
+            <td>
+                <fieldset>
+                    <legend>
+                        <h3>PROFILE</h3>
+                    </legend>
+                    <?php echo 'Name: '.$_COOKIE['name'];?>
+                    <br>
+                    <?php echo 'Email: '.$_COOKIE['email'];?>
+                    <br>
+                    <?php echo 'Gender: '.$_COOKIE['gender'];?>
+                </fieldset>
+            </td>
+        </tr>
+        </table>
+        <table border="1" cellspacing="0" height="25px" width="100%">
+        <tr>
+            <td>
+                <footer style="text-align: center;">Copyright © 2017</footer>
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
